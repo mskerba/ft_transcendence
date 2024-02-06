c32a4d4519aa5207c5dcc536f1ca023c2b6aff95
root <root@LAPTOP-BOG2ULC1>
2024-02-06 18:27:05 +0100

    status

--- front-end/src/Components/Chat/ChatHeader.tsx
+++ front-end/src/Components/Chat/ChatHeader.tsx
@@ -6,30 +6,33 @@ import { useNavigate, Link } from 'react-router-dom';
 import useAxiosPrivate from '../../hooks/UseAxiosPrivate';
 
 
-const ChatHeader = (prop:any) => {
-  const { convInf, setRandomKey, socketRef}: any = useAuth();
-  const group:Boolean = convInf.group;
-  const [status, setStatus] = useState(prop.usersStatus.get(convInf.Id)); 
+const ChatHeader = (prop: any) => {
+  const { convInf, setRandomKey, socketRef }: any = useAuth();
+  const group: Boolean = convInf.group;
+  const [status, setStatus] = useState(prop.usersStatus.get(convInf.Id));
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log(prop.usersStatus.get(convInf.Id));
     setStatus(prop.usersStatus.get(convInf.Id));
     prop.setRefresh(0)
   }, [prop.refresh, prop.usersStatus.get(convInf.Id)]);
 
+  useEffect(() => {
+    console.log("!!!!!>>>>>", status, " " , convInf.status);
+    setStatus(convInf.status);
+  }, [])
 
-  
 
   function handelClick() {
-    if (innerWidth <925)
+    if (innerWidth < 925)
       prop.setShow(1);
   }
 
   function handelClickInf() {
-    prop.setPopupInfParent((prev:any)=> {
-      return ({...prev,display:'flex'})
+    prop.setPopupInfParent((prev: any) => {
+      return ({ ...prev, display: 'flex' })
     });
   }
 
@@ -42,41 +45,41 @@ const ChatHeader = (prop:any) => {
     setRandomKey(generatedName);
     socketRef.current.emit("createPrivateGame", { userId, gameID: generatedName })
     navigate('/game');
-}
+  }
 
   return (
-      <div className='chat-header'>
-        <div className='side-bar-controle-button' onClick={handelClick}>
-          <img src='/src/assets/arrow.svg' className='arrow-svg'/>
-        </div>
-
-        <div className='right-part-header'>
-          {!convInf.group && <Link  to={`/user/${convInf.Id}`}>
-            <div className='chat-info'>
-              <img src={`http://localhost:3000/avatar/${convInf.Avatar}`} className='conversation-avatar'/>
-              <div className='content'>
-                  <h5>{convInf.Name}</h5>
-                  {!group && <p>{status}</p>}
-              </div>
-            </div>
-          </Link>}
-          {convInf.group &&
-            <div className='chat-info'>
-              <img src={`http://localhost:3000/avatar/${convInf.Avatar}`} className='conversation-avatar'/>
-              <div className='content'>
-                  <h5>{convInf.Name}</h5>
-                  {!group && <p>{status}</p>}
-              </div>
-            </div>}
-
-          {group && <div  className='info-group-button' onClick={handelClickInf} ><img src='/src/assets/info-group.svg'/></div>}
-          {!group && <div className='play-button-chat' onClick={() => handlePlayFreindClick(convInf.Id)}>
-              <p>Play Now</p>
-              <img src='/src/assets/play-now.svg' />
-          </div>}
-        </div>
-
+    <div className='chat-header'>
+      <div className='side-bar-controle-button' onClick={handelClick}>
+        <img src='/src/assets/arrow.svg' className='arrow-svg' />
       </div>
+
+      <div className='right-part-header'>
+        {!convInf.group && <Link to={`/user/${convInf.Id}`}>
+          <div className='chat-info'>
+            <img src={`http://localhost:3000/avatar/${convInf.Avatar}`} className='conversation-avatar' />
+            <div className='content'>
+              <h5>{convInf.Name}</h5>
+              {!group && <p>{status}</p>}
+            </div>
+          </div>
+        </Link>}
+        {convInf.group &&
+          <div className='chat-info'>
+            <img src={`http://localhost:3000/avatar/${convInf.Avatar}`} className='conversation-avatar' />
+            <div className='content'>
+              <h5>{convInf.Name}</h5>
+              {!group && <p>{status}</p>}
+            </div>
+          </div>}
+
+        {group && <div className='info-group-button' onClick={handelClickInf} ><img src='/src/assets/info-group.svg' /></div>}
+        {!group && <div className='play-button-chat' onClick={() => handlePlayFreindClick(convInf.Id)}>
+          <p>Play Now</p>
+          <img src='/src/assets/play-now.svg' />
+        </div>}
+      </div>
+
+    </div>
   );
 };
 
